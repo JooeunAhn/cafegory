fc6625a5da4934805345d7b3687aeefc51991272
JooeunAhn <jflow@hanyang.ac.kr>
2016-08-19 04:03:14 +0900

    db 이름 비밀번호 세팅

--- cafegory/cafegory/settings/prod.py
+++ cafegory/cafegory/settings/prod.py
@@ -5,9 +5,9 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DJANGO_DATABASE_ENGINE', 'django.db.backends.postgresql_psycopg2'),
-        'NAME': os.environ.get('DJANGO_DATABASE_NAME', 'hackathon'),
+        'NAME': os.environ.get('DJANGO_DATABASE_NAME', 'cafegory'),
         'USER': os.environ.get('DJANGO_DATABASE_USER', 'ubuntu'),
-        'PASSWORD': os.environ.get('DJANGO_DATABASE_PASSWORD', 'p_rograsv'),
+        'PASSWORD': os.environ.get('DJANGO_DATABASE_PASSWORD', 'google'),
         'HOST': os.environ.get('DJANGO_DATABASE_HOST', '127.0.0.1'),
     }
 }
