2f940c14e91518707a829de026295b77b8ed45be
JooeunAhn <jflow@hanyang.ac.kr>
2016-08-19 00:58:54 +0900

    template

--- cafegory/cafegory/settings.py
+++ cafegory/cafegory/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cafe',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'cafegory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "cafegory", "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
