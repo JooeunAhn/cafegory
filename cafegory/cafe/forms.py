f38f9d79870592a64adf699868a43201f0d53e72
JooeunAhn <jflow@hanyang.ac.kr>
2016-08-19 23:35:35 +0900

    form 수정

--- cafegory/cafe/forms.py
+++ cafegory/cafe/forms.py
@@ -6,7 +6,7 @@ from cafe.widgets import GoogleMapPointWidget
 class CafeForm(forms.ModelForm):
     class Meta:
         model = Cafe
-        fields = ['name', 'is_24', 'is_smoke','is_wifi','is_plug','is_parking','latlng','image',"address","location","end_time"]
+        fields = ['name', 'is_24', 'is_smoke','is_wifi','is_plug','is_parking','latlng','image',"address","location","end_time","tel",]
         widgets = {
             'latlng': GoogleMapPointWidget,
         }
