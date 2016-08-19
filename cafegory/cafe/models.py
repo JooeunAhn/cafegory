caf3578cef3c189fb933dc17003d23fecc9482ad
JooeunAhn <jflow@hanyang.ac.kr>
2016-08-19 23:02:56 +0900

    로케이션 추가 및 아이콘 간 거리 수정 및 상세페이지 수정 및 all 넣음

--- cafegory/cafe/models.py
+++ cafegory/cafe/models.py
@@ -12,6 +12,14 @@ class Cafe(models.Model):
     image = models.ImageField(blank=True, upload_to='cafe_images')
     timeinfo = models.CharField(max_length=100)
     tel = models.CharField(max_length=15)
+    CHOICES = (
+        ("snu","서울대입구"),
+        ("kangnam","강남"),
+        ("hongdae","홍대"),
+        )
+
+    location = models.CharField(max_length=20, choices = CHOICES, default="서울대입구")
+    end_time = models.CharField(max_length=40, default="")
 
     @property
     def lat(self):
--- cafegory/cafe/urls.py
+++ cafegory/cafe/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from cafe.views import index, cafe_detail, cafe_new, cafe_edit
+from cafe.views import index, cafe_detail, cafe_new, cafe_edit, cafe_list
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^(?P<pk>\d+)$', cafe_detail, name="cafe_detail"),
-    url(r'^new/$', cafe_new, name="cafe_new"),
-    url(r'^edit/(?P<pk>\d+)$', cafe_edit, name="cafe_edit")
+    url(r'^$', index, name="index"),
+    url(r'^cafe/(?P<location>[a-z]+)$',cafe_list, name='cafe_list'),
+    url(r'^cafe/(?P<pk>\d+)$', cafe_detail, name="cafe_detail"),
+    url(r'^cafe/new/$', cafe_new, name="cafe_new"),
+    url(r'^cafe/edit/(?P<pk>\d+)$', cafe_edit, name="cafe_edit")
 ]
