478b224210f2888ff9e85f672cdb12c20bad15ed
JooeunAhn <jflow@hanyang.ac.kr>
2016-09-06 15:07:37 +0800

    mypage

--- cafegory/accounts/urls.py
+++ cafegory/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
-
+from .views import mypage
 urlpatterns = [
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, {'next_page': 'cafe:index'}, name='logout'),
+    url(r'^mypage/$', mypage, name="mypage"),
 ]
