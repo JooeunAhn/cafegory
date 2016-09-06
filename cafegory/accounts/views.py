ccd3b015a0715ec941da3750210e409a5352074f
JooeunAhn <jflow@hanyang.ac.kr>
2016-09-06 21:40:31 +0800

    mypage에서 평점및 댓글단 카페 서빙

--- cafegory/accounts/views.py
+++ cafegory/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import Comment_to_us_Form
+from cafe.models import Cafe
+from star_ratings.models import UserRating
 
 # Create your views here.
 
@@ -15,5 +17,9 @@ def mypage(request):
                 form.author = request.user
             form.save()
             messages.success(request,"성공적으로 고객님의 의견이 저장되었습니다. 감사합니다.")
-
-    return render(request,"accounts/mypage.html")
+    comment_cafe_list = Cafe.objects.filter(cafe_comment__author=request.user).distinct()
+    rating_cafe_list = Cafe.objects.filter(ratings__user_ratings__user=request.user).distinct()
+    return render(request,"accounts/mypage.html",{
+        "comment_cafe_list":comment_cafe_list,
+        "rating_cafe_list":rating_cafe_list,
+        })
